95038d80d6e025c6e4f7003838a20fe3bfc81621
Esther Quirós <estherbequiros@gmail.com>
2025-11-30 19:56:36 +0100

    add route landing

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App.jsx";
-
-
+import { BrowserRouter, Routes, Route } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/landing" element={<div>Landing</div>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
